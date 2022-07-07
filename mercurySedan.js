4886bf6a7b81fa8bcf41bf251abd47b566147b3a
ayanbajo <bolatitoayanbajo@gmail.com>
2022-07-06 21:35:43 -0400

    Started Assignment

---
+++ mercurySedan.js
@@ -0,0 +1,35 @@
+
+
+
+class Car extends Vehicle {
+    constructor() {
+        super()
+        this.maximumPassengers = 5
+        this.passenger = 0
+        this.numberOfWheels = 4
+        this.maximumSpeed = 160
+        this.fuel = 10
+        this.scheduleService = false
+    }
+}
+
+loadPassenger(num) {
+    if(this.passenger < maximumPassengers)
+
+}
+
+
+start() {
+    if (this.fuel > 0) {
+        return this.started = true
+    } else {
+        return this.started = false
+    }
+}
+
+scheduleService(mileage) {
+    if (this.mileage > 3000) {
+        this.scheduleService = true
+        return this.scheduleService
+} 
+}
